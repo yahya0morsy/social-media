2cd18801633e9e85231ffe9ee017b373dd281f86
yahya0morsy <ym60307@gmail.com>
2024-10-06 13:39:37 +0300

    third1 commit

--- src/sign.jsx
+++ src/sign.jsx
@@ -28,15 +28,15 @@ function Sign(){
                 </div>
                     <div className="sign1 relative bg-transparent border-4 border-red-700 border-b-0 rounded-b-none rounded-md">
                     <div className='flex flex-row justify-center'><p className='text-2xl text-white'>Sign up</p></div>
-                        <div className='text-white text-base font-medium my-3'>Username:<input type='text' className='rounded-md mx-1'/></div>
+                        <div className='text-white text-sm md:text-base font-medium my-3'>Username:<input type='text' className='rounded-md mx-1'/></div>
                     <div className=''><button onClick={()=>hiderup()}>have an account? sign in</button></div>
                     </div>
                     <div className="sign1 bg-transparent border-4 border-red-700 border-t-0 rounded-t-none rounded-md">
                         <div className='flex flex-row justify-center'><p className='text-2xl text-white'>Sign in</p></div>
-                    <div className='text-white text-lg my-3 mx-2 font-medium'>Username:<input type='text' className='rounded-md mx-1'/></div>
-                    <div className='flex flex-row text-white text-lg my-2 mx-2 font-medium'>password :<input type='password' className='rounded-md mx-1'/><div className=''><button className='text-black rounded-lg hover:cursor-pointer active:translate-x-2 duration-200 active:translate-y-2 active:bg-slate-600 border-2 border-black bg-white'
+                    <div className='text-white text-base sm:text-lg my-3 mx-2 font-medium'>Username:<input type='text' className='rounded-md mx-1 text-black'/></div>
+                    <div className='flex flex-row text-white text-base sm:text-lg my-2 mx-2 font-medium'>password :<input type='password' className='rounded-md mx-1'/><div className=''><button className='text-black rounded-lg hover:cursor-pointer active:translate-x-2 duration-200 active:translate-y-2 active:bg-slate-600 border-2 border-black bg-white'
                                                                                                                                                                                  onClick={()=>navigate('/home')}>submit</button></div></div>
-                    <div className='mx-2 text-blue-500 hover:cursor-pointer'>forger password ?</div>
+                    <div className='mx-2 text-blue-500 text-base sm:text-lg hover:cursor-pointer'>forger password ?</div>
                     <div className='flex flex-row justify-center'><button onClick={()=>hiderdw()}>dont have an account? register</button></div>
                     </div>
                 </div>
